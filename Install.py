4e1ad4e45a2398360e66aa9e602457b339d7a6bc
Gpknoob <44008883+Gpknoob@users.noreply.github.com>
2018-10-10 05:18:06 +0530

    Install.py
    All first open up your termux
    Write command: apt-get install git
    Enter
    Then write command: git clone past my url here
    enter
    change directory's to package folder
    Then run the command: python Install.py
    Enter

---
+++ Install.py
@@ -0,0 +1,13 @@
+#!/data/data/com.termux/files/usr/bin/python
+import os
+print("this is created by Abhishek")
+print("this command is used to install all the requirements package on termux")
+os.system("apt-get update -y")
+os.system("apt-get -y upgrade")
+os.system("apt-get install -y ruby")
+os.syatem("apt-get install -y perl")
+os.system("apt-get install -y python2")
+os.system("apt-get install -y nano")
+os.system("apt-get install -y wget")
+os.system("apt-get install -y w3m")
+os.system("apt-get install -y ssh")
